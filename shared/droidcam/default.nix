b1a9cc27a009b1f7356a81e6142d849362970df6
Ratyuha <ratmir06062009@gmail.com>
2026-04-06 15:22:45 +0200

    ww

--- shared/droidcam/default.nix
+++
@@ -1,9 +0,0 @@
-{ pkgs, config, lib, ... }: {
-  let 
-    v4l2loopback-dc = config.boot.kernelPackages.callPackage ./v4l2loopback-dc.nix { };
-    droidcam = pkgs.callPackage ./droidcam.nix {};
-  in {
-    boot.extraModulePackages = [ v4l2loopback-dc ];
-    environment.systemPackages = [ droidcam ];
-  }
-}
